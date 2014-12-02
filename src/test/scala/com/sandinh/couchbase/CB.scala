e5ef8d7163fff14fb8e1e0c328b11db45988faab
giabao <giabao@sandinh.net>
2014-12-02 11:34:35 +0700

    add a fail test for http://www.couchbase.com/issues/browse/JCBC-642

--- src/test/scala/com/sandinh/couchbase/CB.scala
+++ src/test/scala/com/sandinh/couchbase/CB.scala
@@ -7,4 +7,5 @@ import com.typesafe.config.Config
 @Singleton
 class CB @Inject() (config: Config, val cluster: CBCluster) {
   lazy val bk1 = cluster.openBucket("bk1", JsTranscoder)
+  lazy val bk2 = cluster.openBucket("acc")
 }
---
+++ src/test/scala/com/sandinh/couchbase/JCBC_642Spec.scala
@@ -0,0 +1,19 @@
+package com.sandinh.couchbase
+
+import com.couchbase.client.java.document.JsonStringDocument
+import scala.concurrent.duration._
+
+class JCBC_642Spec extends GuiceSpecBase {
+  "CBCluster" should {
+    "pass JCBC-642" in {
+      val content = "test_value_JCBC_642"
+      val doc = JsonStringDocument.create("test_key_JCBC_642", 10000, content)
+      cb.bk1.upsert(doc).flatMap { d =>
+        cb.bk2.upsert(d)
+      }.timeout(5.seconds)
+        .toBlocking
+        .single
+        .content === content
+    }
+  }
+}
