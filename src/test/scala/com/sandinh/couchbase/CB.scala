0068ab8015120fe45c78e9818709c83608f3b697
giabao <giabao@sandinh.net>
2015-07-09 00:39:55 +0700

    cleanup a test class

--- src/test/scala/com/sandinh/couchbase/CB.scala
+++ src/test/scala/com/sandinh/couchbase/CB.scala
@@ -1,10 +1,9 @@
 package com.sandinh.couchbase
 
 import javax.inject._
-import com.typesafe.config.Config
 
 @Singleton
-class CB @Inject() (config: Config, val cluster: CBCluster) {
+class CB @Inject() (val cluster: CBCluster) {
   lazy val bk1 = cluster.openBucket("bk1")
   lazy val bk2 = cluster.openBucket("acc")
 }
