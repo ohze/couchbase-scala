60997aa4db5af4a1ad303aad5959793d59549fe6
thanhbv <thanhbv@sandinh.net>
2017-09-11 01:07:52 +0700

    fix deprecated warnings when upgrade to play 2.6.x

--- play/src/test/scala/com/sandinh/couchbase/PlayCBClusterSpec.scala
+++ play/src/test/scala/com/sandinh/couchbase/PlayCBClusterSpec.scala
@@ -7,8 +7,8 @@ class PlayCBClusterSpec extends Specification {
   "PlayCBCluster" >> {
     val app = new GuiceApplicationBuilder().build()
 
-    app.configuration.getString("com.sandinh.couchbase.buckets.bk1.name") must beSome("fodi")
-    app.configuration.getStringSeq("play.modules.enabled").get must contain("com.sandinh.couchbase.PlayModule")
+    app.configuration.getOptional[String]("com.sandinh.couchbase.buckets.bk1.name") must beSome("fodi")
+    app.configuration.get[Seq[String]]("play.modules.enabled") must contain("com.sandinh.couchbase.PlayModule")
 
     app.injector.instanceOf[CBCluster] must beAnInstanceOf[PlayCBCluster]
   }
