57307a9b7cf358cbe3afca9eec491ceb24faaa16
thanhbv <thanhbv@sandinh.net>
2017-09-11 01:19:30 +0700

    fix error: ``` A binding to com.typesafe.config.Config was already configured at play.api.inject.BuiltinModule ``` (play 2.6.x already bind Config)

--- play/src/main/scala/com/sandinh/couchbase/PlayModule.scala
+++ play/src/main/scala/com/sandinh/couchbase/PlayModule.scala
@@ -1,12 +1,10 @@
 package com.sandinh.couchbase
 
-import com.typesafe.config.Config
 import play.api.{Configuration, Environment}
 import play.api.inject.Module
 
 class PlayModule extends Module {
   def bindings(environment: Environment, configuration: Configuration) = Seq(
-    bind[Config].toInstance(configuration.underlying),
     bind[CBCluster].to[PlayCBCluster]
   )
 }
