8145dba4410425e9d51c240c609d104ef0537f6a
thanhpv <thanhpv@sandinh.net>
2021-08-20 17:19:24 +0700

    Update library sbt-sonatype

--- project/MySonatype.scala
+++ project/MySonatype.scala
@@ -5,6 +5,7 @@ import xerial.sbt.Sonatype.SonatypeKeys._
 object MySonatype {
   lazy val settings = Seq(
     publishMavenStyle := true,
+    pomIncludeRepository := { _ => false },
     publishTo := sonatypePublishToBundle.value,
     licenses := Seq("Apache 2" -> url("http://www.apache.org/licenses/LICENSE-2.0")),
     homepage := Some(url("https://github.com/ohze/couchbase-scala")),
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,4 @@
-addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "3.8.1")
-addSbtPlugin("com.jsuereth" % "sbt-pgp" % "2.0.0")
+addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "3.9.5")
+addSbtPlugin("com.github.sbt" % "sbt-pgp" % "2.1.2")
 addSbtPlugin("io.get-coursier" % "sbt-coursier" % "1.0.3")
 //addSbtPlugin("com.lucidchart" % "sbt-scalafmt-coursier" % "1.15")
