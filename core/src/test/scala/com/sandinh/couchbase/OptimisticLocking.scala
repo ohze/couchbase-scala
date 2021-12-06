341a21c2dc25bf0b56bd2737d214443cf60150a9
Bùi Việt Thành <thanhbv@sandinh.net>
2021-12-06 23:53:30 +0700

    Clean OptimisticLocking

--- core/src/test/scala/com/sandinh/couchbase/OptimisticLocking.scala
+++ core/src/test/scala/com/sandinh/couchbase/OptimisticLocking.scala
@@ -3,7 +3,7 @@ package com.sandinh.couchbase
 import com.couchbase.client.core.error.CasMismatchException
 import javax.inject.Inject
 import scala.concurrent.duration._
-import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.Await
 
 class OptimisticLocking extends GuiceSpecBase {
   @Inject private var trophyCao: TrophyCao = null
@@ -16,14 +16,7 @@ class OptimisticLocking extends GuiceSpecBase {
         Await.result(trophyCao.getWithCAS(key), 2.second)
       val t2 = trophyCao.replace(key, Trophy.t2, old._2)
       val t3 = trophyCao.replace(key, Trophy.t3, old._2)
-      RichFuture.zip(t2, t3) must throwAn[CasMismatchException].await
+      t2.zip(t3) must throwAn[CasMismatchException].await
     }
   }
-}
-
-object RichFuture {
-  def zip[A, B](a: Future[A], b: Future[B])(
-    implicit ec: ExecutionContext
-  ): Future[(A, B)] =
-    Future.sequence(Seq(a, b)).map(_ => (a.value.get.get, b.value.get.get))
 }
